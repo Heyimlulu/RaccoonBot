a085b16db3fd16ffb699cd0f318f774db6f9a098
Lucas <61554619+Heyimlulu@users.noreply.github.com>
2021-11-08 21:37:16 +0100

    Add welcome and leave message [WIP]

---
+++ event/listeners/guildMemberAdd.js
@@ -0,0 +1,54 @@
+const { Listener } = require('discord-akairo');
+const joinChannel = require('../../models').joinChannel;
+
+class guildMemberAddListener extends Listener {
+    constructor() {
+        super('guildMemberAdd', {
+            emitter: 'client',
+            event: 'guildMemberAdd'
+        });
+    }
+
+    async exec(member) {
+
+        /*
+        const welcome = await joinChannel.findOne({
+            where: { guildID: member.guild.id }
+        });
+
+        console.log(member.guild.id);
+
+         */
+
+        // if (welcome) {}
+        if (member.guild.id == '842856705029046282') {
+            //const channel = this.client.channels.resolve(welcome.get('channelID'));
+            const channel = this.client.channels.resolve('881623587335860275');
+
+            /*
+            let welcomeMessage = welcome.get('message');
+
+            let invite = new RegExp(/(https?:\/\/)?(www\.)?discord(?:app\.com|\.gg)[/invite/]?(?:(?!.*[Ii10OolL]).[a-zA-Z0-9]{5,6}|[a-zA-Z0-9-]{2,32})/g);
+
+            let username = member.user.username;
+            let user = member.user;
+            if (username.match(invite)) {
+                username = username.replace(/(https?:\/\/)?(www\.)?discord(?:app\.com|\.gg)[/invite/]?(?:(?!.*[Ii10OolL]).[a-zA-Z0-9]{5,6}|[a-zA-Z0-9-]{2,32})/g, '[REDACTED]');
+                user = username;
+            }
+
+            welcomeMessage = welcomeMessage.replace(/\[member\]/g, username);
+            welcomeMessage = welcomeMessage.replace(/\[memberPing\]/g, user);
+            welcomeMessage = welcomeMessage.replace(/\[server\]/g, member.guild.name);
+
+            return channel.send(welcomeMessage);
+
+             */
+
+            return channel.send(`${member.user} is curious and just joined the server 🦝`);
+        }
+
+    }
+}
+
+module.exports = guildMemberAddListener;
---
+++ event/listeners/guildMemberRemove.js
@@ -0,0 +1,51 @@
+const { Listener } = require('discord-akairo');
+const leaveChannel = require('../../models').leaveChannel;
+
+class guildMemberAddListener extends Listener {
+    constructor() {
+        super('guildMemberRemove', {
+            emitter: 'client',
+            event: 'guildMemberRemove'
+        });
+    }
+
+    async exec(member) {
+
+        /*
+        const bye = await leaveChannel.findOne({
+            where: { guildID: member.guild.id }
+        });
+
+         */
+
+        // if (bye) {}
+        if (member.guild.id == '842856705029046282') {
+            //const channel = this.client.channels.resolve(bye.get('channelID'));
+            const channel = this.client.channels.resolve('881623587335860275');
+
+            /*
+            let leaveMessage = bye.get('message');
+
+            let invite = new RegExp(/(https?:\/\/)?(www\.)?discord(?:app\.com|\.gg)[/invite/]?(?:(?!.*[Ii10OolL]).[a-zA-Z0-9]{5,6}|[a-zA-Z0-9-]{2,32})/g);
+
+            let username = member.user.username;
+            let user = member.user;
+            if (username.match(invite)) {
+                username = username.replace(/(https?:\/\/)?(www\.)?discord(?:app\.com|\.gg)[/invite/]?(?:(?!.*[Ii10OolL]).[a-zA-Z0-9]{5,6}|[a-zA-Z0-9-]{2,32})/g, '[REDACTED]');
+                user = username;
+            }
+
+            leaveMessage = leaveMessage.replace(/\[member\]/, username);
+            leaveMessage = leaveMessage.replace(/\[memberPing\]/, user);
+            leaveMessage = leaveMessage.replace(/\[server\]/, member.guild.name);
+
+            return channel.send(leaveMessage);
+
+             */
+
+            return channel.send(`${member.user} just leave the server 🦝`);
+        }
+    }
+}
+
+module.exports = guildMemberAddListener;
